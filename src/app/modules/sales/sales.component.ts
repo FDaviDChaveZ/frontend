42f81650d27151b3a7990fddf09daee5ae37f350
Sergio Valles <sergivalles113@gmail.com>
2024-08-13 13:57:59 -0500

    Mensajes y limpieza del formulario en el modulo de ventas

--- src/app/modules/sales/sales.component.ts
+++ src/app/modules/sales/sales.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Sales } from 'src/app/core/models/sales/sales';
 import { SalesService } from 'src/app/core/services/sales/sales.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-sales',
@@ -9,7 +10,7 @@ import { SalesService } from 'src/app/core/services/sales/sales.service';
   styleUrls: ['./sales.component.scss']
 })
 export class SalesComponent {
-  constructor(private salesService: SalesService) { }
+  constructor(private salesService: SalesService, private toastr: ToastrService) { }
 
   salesForm: FormGroup = new FormGroup({
     productId: new FormControl('', Validators.required),
@@ -27,9 +28,12 @@ export class SalesComponent {
     const data: Sales = this.salesForm.value;
     this.salesService.GenerateSale(data).subscribe({
       next: (response) => {
+        this.toastr.success('La venta se registro exitosamente','Venta Registrada');
+        this.salesForm.reset();
         console.log(response);
       },
       error: (error) => {
+        this.toastr.error('Ocurrio un error al registrar la venta', 'Error');
         console.log(error);
       }
     });
